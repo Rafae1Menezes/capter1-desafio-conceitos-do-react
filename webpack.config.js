9987351c73da37795c8f8836b2ee57e237f75e45
Rafael Menezes <menezes.inbox@gmail.com>
2021-12-27 15:15:02 -0300

    atualiza webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
-      publicPath: 'auto',
    },
    resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
